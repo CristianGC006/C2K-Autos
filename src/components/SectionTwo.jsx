4fdd536832ef36d413f0aa74b38685361b325ab9
CristianGC006 <cogolloguzmanesteban@gmail.com>
2025-03-20 01:43:59 -0500

    Feat(ComponentTwo):Se creo la utilidad de poder seleccionar hacia donde quiero consultar la disponibilidad  de mi vehiculo, junto con sus respectivos estilos y propiedades de JavaScript

--- src/components/SectionTwo.jsx
+++ src/components/SectionTwo.jsx
@@ -2,14 +2,24 @@ import './components.css';
 import carsCamaro from '../assets/carsCamaro.png'
 import carsCharger from '../assets/carsCharger.png'
 import CarsCard from './CarsCard';
+import MultiFunctionalButton from './MultiFunctionalButton';
+import MultiFunctionalButtonV2 from './MultiFunctionalButtonV2';
 function SectionTwo(){
     return(
         <>
+        <section className='cars-main'>
         <div className='section-two-carsContainer'>
             <CarsCard carImg={carsCamaro} />
             <CarsCard carImg={carsCamaro} />
             <CarsCard carImg={carsCharger} />
         </div>
+        </section>
+        <section className='search-bar'>
+            <MultiFunctionalButton type='input' label={"Escribe tu lugar de recogida"}/>
+            <MultiFunctionalButtonV2 type='date' label='Fecha' icon='' />
+            <MultiFunctionalButtonV2 type='time' label='Hora' icon='' />
+            <button type='submit' className='search-button'>Consultar Disponibilidad</button>
+        </section>
         </>
     )
 
