250a943ad51a70ba9dc43cf471bb03a3a327845c
KalebParra007 <kalebparramarin135@gmail.com>
2025-04-24 18:51:36 -0500

    feat(Login): Reestructurar el formulario de inicio de sesión y registro; añadir logo y nuevos campos de entrada.

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -4,6 +4,7 @@ import { genericAlert,
   redirectionAlert
  } from "../../helpers/functions"
 import { useNavigate } from 'react-router-dom';
+import Logo from "../../assets/C2K-LogoNoBackground.png"
 
 let urlCustomers = "https://fakeapic2k.onrender.com/Customers";
 
@@ -49,34 +50,71 @@ function logIn(){
 }
 
   return (   
-      <div className="wrapper">
-        <div className="card-switch">
-          <label className="switch">
-            <input type="checkbox" className="toggle" />
-            <span className="slider" />
-            <span className="card-side" />
-            <div className="flip-card__inner">
-              <div className="flip-card__front">
-                <div className="title">Log in</div>
-                <form className="flip-card__form" action>
-                  <input onChange={(e) => setEmail(e.target.value)} className="flip-card__input" name="email" placeholder="Email" type="email" />
-                  <input onChange={(e) => setPassword(e.target.value)} className="flip-card__input" name="password" placeholder="Password" type="password" />
-                  <button onClick={logIn} type='button' className="flip-card__btn">Let`s go!</button>
-                </form>
-              </div>
-              <div className="flip-card__back">
-                <div className="title">Sign up</div>
-                <form className="flip-card__form" action>
-                  <input className="flip-card__input" placeholder="Name" type="name" />
-                  <input className="flip-card__input" name="email" placeholder="Email" type="email" />
-                  <input className="flip-card__input" name="password" placeholder="Password" type="password" />
-                  <button className="flip-card__btn">Confirm!</button>
-                </form>
-              </div>
-            </div>
-          </label>
-        </div>   
-      </div>
+      <section className='container_form'>
+
+                <form className="login_form" action>
+                <div className="logo_login">
+                  <img src={Logo} alt="LogoC2K" />
+                </div>
+                  <input onChange={(e) => setEmail(e.target.value)} className="login_input" id='email' name="email" placeholder="Email" type="email" />
+                  <input onChange={(e) => setPassword(e.target.value)} className="login_input"  name="password" placeholder="Password" type="password" />
+                  //El boton de iniciar sesion lleva onClick
+                </form> 
+            
+               <div className="title">Registro</div>
+                <form className="register_form" action>
+                  <div className='div_register'>
+                  <input className="register_input" placeholder="Nombre" type="name" />
+                  </div>
+                  <div className="div_register">
+                  <input className="register_input" name="email" placeholder="Email" type="email" />
+                  </div>
+                  <div className="div_register">
+                  <input className="register_input" placeholder="Apellidos" type="name" />
+                  </div>
+                  <div className="div_register">
+                  <input className="register_input" placeholder="Número de Telefono" type="tel" />
+                  </div>
+                  <div className="div_register">
+                  <select id='document_type' className="register_input">
+                    <option value="Tipo de Documento">Tipo de Documento</option>
+                    <option value="Cedula de Ciudadania">Cedula de ciudadania</option>
+                    <option value="Cedula de extranjeria">Cedula de extranjeria</option>
+                    <option value="Pasaporte">Pasaporte</option>
+                    </select>
+                  </div>
+                  <div className="div_register">
+                  <select id="Sex" className='register_input'>
+                    <option value="Sexo">Sexo</option>
+                    <option value="Masculino">Masculino</option>
+                    <option value="Femenino">Femenino</option>
+                    <option value="Otro">Otro</option>
+                  </select>
+                  </div>
+                  <div className="div_register">
+                  <input className="register_input" placeholder="Número de Documento" type="text"/>
+                  </div>
+                  <div className="div_register">
+                  <input className="register_input" placeholder="Nacionalidad" type="text" />
+                  </div>
+                  <div className="div_register">
+                  <input className="register_input" name="password" placeholder="Contraseña" type="password" />
+                  </div>
+                  <div id='span' className="div_register">
+                  <span>¿Tiene licencia de conducción?</span>
+                  <label htmlFor="license">Si</label>
+                  <input type="checkbox" />
+                  <label htmlFor="license">No</label>
+                  <input type="checkbox" />
+                  </div>
+                  <div className="div_register">
+                  <input className="register_input" name="password_confirmation" placeholder="Confirmar Contraseña" type="password" />
+                  </div>
+                  <div className="div_register">
+                  <input className="register_input" placeholder="Número de licencia" type="text" />
+                  </div>
+                </form> 
+                </section>   
   );
 }
 export default Login;
